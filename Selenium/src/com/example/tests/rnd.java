4e7a679086f650db55a3a10ef9f3bdd09a33960d
Poschev38 <poschev38@gmail.com>
2017-04-27 05:27:09 +0800

    Update rnd.java

--- Selenium/src/com/example/tests/rnd.java
+++ Selenium/src/com/example/tests/rnd.java
@@ -21,29 +21,40 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class rnd 
 {
-	
 	WebDriver selenium = new FirefoxDriver();
 	
-    @Test
-    public void test() throws InterruptedException
-    { 
-     	selenium.get("https://grill-man.ru/catalog");
-     		
-     	//List<WebElement> nodes = selenium.findElements(By.cssSelector
-     		//	("#content [class=node node-catalog node-promoted clearfix"));
-     				 
- 	    		List<WebElement> myList=selenium.findElements(By.cssSelector
- 	    				("input[value=Заказать]"));
- 	    		
- 	    		List<WebElement> all_elements=new ArrayList<WebElement>();
- 	    		
-    		    for(int i=0; i<myList.size(); i++)
-    		    {
-			Thread.sleep(2000);
-    		    	all_elements.add(myList.get(i));
-    		    	myList.get(i).click();
-    		    	System.out.println(all_elements);
-    		    }			  
+	    @Test
+	    public void test() throws InterruptedException
+	    { 
+		selenium.get("https://grill-man.ru/catalog");
+
+		    //ноды
+		//List<WebElement> nodes = selenium.findElements(By.cssSelector
+			//("#content [class=node node-catalog node-promoted clearfix"));
+
+			//Кнопки
+		    	List<WebElement> myList=selenium.findElements(By.cssSelector("input[value=Заказать]"));
+			//массив
+		    	List<WebElement> all_elements=new ArrayList<WebElement>();
+
+			    for(int i=0; i<myList.size(); i++)
+			    {
+				Thread.sleep(2000);
+				all_elements.add(myList.get(i)); // загрузка в массив 
+				myList.get(i).click();
+				System.out.println(all_elements);
+			    }			
+
+			 WebElement zakazat = selenium.findElement(By.cssSelector(".checkout-button"));
+			 zakazat.click();
+
+			 WebElement number = (new WebDriverWait(selenium, 10))
+						 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector
+								 ("#edit-customer-profile-billing-field-phone-und-0-value")));
+			 selenium.findElement(By.id("#edit-customer-profile-billing-field-phone-und-0-value")).sendKeys("88005553535");
+
+			 WebElement oformitZakaz = selenium.findElement(By.cssSelector("#edit-continue"));
+			 oformitZakaz.click();
     		    
     			
    		    
